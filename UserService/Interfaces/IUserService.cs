29f21f0d6a0243385ce6c796efa08a2bcad00b80
farhannizami <farhannizami0612@gmail.com>
2025-05-05 00:03:51 +0600

    Added basic user functionalities

---
+++ UserService/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using UserService.DTOs;
+
+namespace UserService.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserResponse> CreateUserAsync(CreateUserRequest request);
+        Task<UserResponse?> GetUserByIdAsync(int Id);
+        Task<List<UserResponse>> GetAllUsersAsync();
+    }
+}
---
+++ UserService/Services/UserService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Data;
+using UserService.DTOs;
+using UserService.Interfaces;
+using UserService.Models;
+
+namespace UserService.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserDbContext _context;
+        public UserService(UserDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<UserResponse> CreateUserAsync(CreateUserRequest request)
+        {
+            var user = new User
+            {
+                Name = request.Name,
+                Email = request.Email,
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new UserResponse
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email
+            };
+        }
+
+        public async Task<List<UserResponse>> GetAllUsersAsync()
+        {
+            return await _context.Users
+                .Select(user => new UserResponse
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Email = user.Email
+                }).ToListAsync();
+        }
+
+        public async Task<UserResponse?> GetUserByIdAsync(int Id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == Id);
+            
+            if (user == null) return null;
+
+            return new UserResponse
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email
+            };
+        }
+    }
+}
