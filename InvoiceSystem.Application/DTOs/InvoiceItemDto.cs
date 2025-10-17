e5df3a292270ec21de1e0bb719552b6e8d778160
farhannizami <farhannizami0612@gmail.com>
2025-10-18 05:16:39 +0600

    #7 Add mapper and CustomerService

--- InvoiceSystem.Application/DTOs/InvoiceItemDto.cs
+++ InvoiceSystem.Application/DTOs/InvoiceItemDto.cs
@@ -2,10 +2,10 @@
 {
     public class InvoiceItemDto
     {
-        public Guid Id { get; private set; }
-        public string Description { get; private set; }
-        public int Quantity { get; private set; }
-        public decimal UnitPrice { get; private set; }
-        public decimal LineTotal => Quantity * UnitPrice;
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal LineTotal { get; set; }
     }
 }
